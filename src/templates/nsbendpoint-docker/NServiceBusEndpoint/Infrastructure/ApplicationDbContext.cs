d91f3c0791759676a011e61df26f0be7c14d40eb
Stefan Vogelsang <s.vogelsang@ams-erp.com>
2022-09-07 19:20:19 +0200

    Added NServiceBus Endpoint template.

---
+++ src/templates/nsbendpoint-docker/NServiceBusEndpoint/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBusEndpoint.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public const string DefaultSchema = "NServiceBusEndpoint";
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(DefaultSchema);
+    }
+}
